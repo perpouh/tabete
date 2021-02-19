fe6adbee1fcfa22ce7ded8b4da4c7deeff6482be
perpouh <perpouh@gmail.com>
2021-02-19 15:33:41 +0900

    stores#show作成

---
+++ server/app/controllers/stores_controller.rb
@@ -0,0 +1,10 @@
+class StoresController < ApplicationController
+  before_action :set_store, only: [:show]
+  def show; end
+
+  private
+
+  def set_store
+    @store = Store.find(params[:id])
+  end
+end
---
+++ server/app/views/stores/show.json.jbuilder
@@ -0,0 +1,4 @@
+json.store do
+  json.merge! @store.attributes
+  json.official_accounts @store.official_accounts
+end
